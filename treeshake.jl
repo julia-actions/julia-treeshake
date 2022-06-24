509fa7a74411e20585f4a89d77c2ea05ee2344f7
Ian Butterworth <i.r.butterworth@gmail.com>
2022-06-23 21:32:47 -0400

    Add more info and a success message (#2)
    * add more info and a success message
    
    * fixes
    
    * add counts
    
    * restore error exit
    
    * more counts
    
    * add __init__ caveat note
    
    * add warning if codecov is entirely missing
    
    * Update README.md
    
    * more formatting etc.
    
    * more notes

--- treeshake.jl
+++ treeshake.jl
@@ -21,21 +21,41 @@ for dir in depot_packages
     end
 end
 
-deps = copy(Pkg.Types.Context().env.project.deps)
-for dep in deps
+direct_deps = Pkg.Types.Context().env.project.deps
+unused_direct_deps = String[]
+for dep in direct_deps
     pkgname = first(dep)
     if !haskey(found_dict, pkgname) || found_dict[pkgname]
-        delete!(deps, first(dep))
+        push!(unused_direct_deps, pkgname)
     end
 end
 
-if !isempty(deps)
-    @error "Some direct dependencies were not used by the test code" deps
+indirect_deps = [last(kv).name for kv in Pkg.Types.Context().env.manifest.deps]
+unused_indirect_deps = String[]
+for pkgname in indirect_deps
+    if !haskey(found_dict, pkgname) || found_dict[pkgname]
+        push!(unused_indirect_deps, pkgname)
+    end
+end
 
-    @warn """This information should be used with caution.
-    The check is only _at best_ as good as the coverage of the package tests, or provided test script.
-    Also consider that the setup of the CI machine may impact coverage, with platform-guarded code
-    usage hiding real code usage on other platforms.
-    """
+if length(unused_indirect_deps) == length(indirect_deps)
+    @warn "No coverage files detected. Are you sure you ran code with at least coverage in `user` mode?"
+end
+
+@warn """This information should be used with some caution.
+- The check is only _at best_ as good as the coverage of the package tests, or provided test script.
+- Consider that the setup of the CI machine may impact coverage, with platform-guarded code usage hiding real code usage on other platforms.
+- Currently any `__init__()` calls within packages will mark them as used, even if they are only imported.
+"""
+
+if isempty(unused_direct_deps) && isempty(unused_indirect_deps)
+    @info "All $(length(direct_deps)) direct and $(length(indirect_deps)) indirect dependencies were used"
+else
+    if !isempty(unused_direct_deps)
+        @info """$(length(unused_direct_deps)) of $(length(direct_deps)) direct dependencies were not used by the test code: \n  $(join(sort(unused_direct_deps), "\n  "))"""
+    end
+    if !isempty(unused_indirect_deps)
+        @info """$(length(unused_indirect_deps)) of $(length(indirect_deps)) indirect dependencies were not used by the test code: \n  $(join(sort(unused_indirect_deps), "\n  "))"""
+    end
     exit(1)
 end
